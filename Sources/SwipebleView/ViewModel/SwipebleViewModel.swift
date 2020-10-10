e7d7a42d5daf7a4e60020a289e936e298a3963e2
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-10 15:16:28 +0200

    public protocol

--- Sources/SwipebleView/ViewModel/SwipebleViewModel.swift
+++ Sources/SwipebleView/ViewModel/SwipebleViewModel.swift
@@ -8,7 +8,7 @@
 import Combine
 import UIKit
 
-protocol SwipebleViewModel: ObservableObject {
+public protocol SwipebleViewModel: ObservableObject {
     var dragOffset: CGSize {get set} // default CGSize.zero
     var actions: EditActionsVM {get set} //  = EditActionsVM([])
 }
