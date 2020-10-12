345b3162e3fabef401542071dd39d75422b53192
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-12 15:11:53 +0200

    bg color removed

--- Sources/SwipebleView/View/SwipebleView.swift
+++ Sources/SwipebleView/View/SwipebleView.swift
@@ -8,60 +8,54 @@ public struct SwipebleView<T,Content: View>: View  where T: SwipebleViewModel{
     @ObservedObject var viewModel: T
     @State var frame: CGSize = .zero
     @State private var actions: EditActions?
-    @State private var backGroundColor: Color?
-
+    
     let content: Content
     
-    public init(@ViewBuilder content: () -> Content, viewModel: T, backGroundColor: Color) {
+    public init(@ViewBuilder content: () -> Content, viewModel: T) {
         self.content = content()
         self.viewModel = viewModel
-        self.backGroundColor = backGroundColor
     }
     
     private func makeView(_ geometry: GeometryProxy) -> some View {
         print(geometry.size.width, geometry.size.height)
-
         DispatchQueue.main.async { self.frame = geometry.size }
-        
         return content
-            .frame(maxHeight: .infinity)
     }
     
     public var body: some View {
         
         return
             ZStack {
-
+                
                 EditActions(viewModel: viewModel.actions)
                 GeometryReader { reader in
                     self.makeView(reader)
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
                         .offset(x: viewModel.dragOffset.width)
                 }
-                    .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
-                        withAnimation {
-                            viewModel.dragOffset = CGSize.zero
-                        }
-                    })
-                    
-                    .gesture(
-                        DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
-                            .onEnded { value in
-                                
-                                withAnimation {
-                                    print(viewModel.dragOffset)
-                                    if value.translation.width < 0 && value.translation.height > -30 && value.translation.height < 30 {
-                                        viewModel.dragOffset = (CGSize.init(width: (CGFloat(min(4, viewModel.actions.actions.count)) * -80 - 10), height: 0))
-                                       
-                                    }
-                                    else if value.translation.width > 0 && value.translation.height > -30 && value.translation.height < 30 {
-                                        viewModel.dragOffset = CGSize.zero
-                                    }
+                .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                    withAnimation {
+                        viewModel.dragOffset = CGSize.zero
+                    }
+                })
+                
+                .gesture(
+                    DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
+                        .onEnded { value in
+                            
+                            withAnimation {
+                                print(viewModel.dragOffset)
+                                if value.translation.width < 0 && value.translation.height > -30 && value.translation.height < 30 {
+                                    viewModel.dragOffset = (CGSize.init(width: (CGFloat(min(4, viewModel.actions.actions.count)) * -80 - 10), height: 0))
+                                    
+                                }
+                                else if value.translation.width > 0 && value.translation.height > -30 && value.translation.height < 30 {
+                                    viewModel.dragOffset = CGSize.zero
                                 }
                             }
-                    )
+                        }
+                )
             }
-            .background(backGroundColor)
     }
 }
 
@@ -83,7 +77,7 @@ struct SwipebleView_Previews: PreviewProvider {
                     }.padding()
                     .background(Color.blue)
                     
-                }, viewModel: example(), backGroundColor: .red)
+                }, viewModel: example())
                 
                 Spacer()
             }
