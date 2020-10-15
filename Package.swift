6d956773a53a9c5c432f9122fc702f8e6bf6818c
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-15 13:44:51 +0200

    Github actions setup swift-tools-version:5.3 => swift-tools-version:5.2 .iOS(.v14) => .iOS(.v13)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwipeableView",
     platforms: [
         // Add support for all platforms starting from a specific version.
-        .macOS(.v10_15), .iOS(.v14)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
