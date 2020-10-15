60523dbf95ac821a95fdb1b88c4f29bd52f619a2
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-15 16:00:01 +0200

    swipableviewTests added

--- Tests/SwipeableViewTests/SwipeableViewTests.swift
+++ Tests/SwipeableViewTests/SwipeableViewTests.swift
@@ -1,15 +1,74 @@
 import XCTest
+import Combine
+import SwiftUI
 @testable import SwipeableView
 
 final class swipableviewTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(swipableview().text, "Hello, World!")
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    func testAction() {
+        let action = Action(title: "Foo", iconName: "trash", bgColor: .delete, action: { print("Foo") })
+        
+        XCTAssertNotNil(action)
+        
+        XCTAssertNotNil(action.action)
+        
+        XCTAssert(action.title == "Foo")
+        XCTAssert(action.iconName == "trash")
+        XCTAssert(action.bgColor == .delete)
+        
+    }
+    
+    func testSWViewModel() {
+        let size = CGSize.init(width: 100, height: 100)
+        let model = SWViewModel(state: ViewState.center, size: size)
+        
+        XCTAssertNotNil(model)
+        XCTAssertNotNil(model.id)
+        
+        XCTAssert(model.state == .center)
+        
+        model.state = .left
+        XCTAssert(model.state == .left)
+        
+        
+        model.state = .right
+        XCTAssert(model.state == .right)
+
+       // model.goToCenter()
+        
+    }
+    
+    func testSWViewModelDidChange() {
+        
+        let size = CGSize.init(width: 100, height: 100)
+        let model = SWViewModel(state: ViewState.center, size: size)
+        var didChange = false
+
+        model.stateDidChange.sink(receiveValue: { vm in
+            didChange = true
+      
+        }).store(in: &subscriptions)
+        
+        model.state = .left
+        XCTAssert(didChange)
+        didChange = false
+        
+        model.state = .center
+        XCTAssert(!didChange)
+        
+        model.state = .right
+        XCTAssert(didChange)
+        
     }
 
+    
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testAction", testAction),
+        ("testSWViewModel", testSWViewModel),
+        ("testSWViewModelDidChange", testSWViewModelDidChange)
+        
     ]
 }
